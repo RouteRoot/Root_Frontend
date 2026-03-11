e2d4f2badad5b89d4542d147b9ad8f3620952296
min3eo <110177217+min3eo@users.noreply.github.com>
2026-03-12 00:04:18 +0900

    feat: 네틀리파이 배포 수정

--- app/api/axios/axiosInstance.ts
+++ app/api/axios/axiosInstance.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://13.239.246.72:8080/api"
-    : "api/proxy";
-//엔드포인트 변경
+    : "api/";
 
 export const axiosInstance = axios.create({
   baseURL,
