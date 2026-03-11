2ad2af1b0d1e93dd3d2c46492d140a88078abb2a
min3eo <110177217+min3eo@users.noreply.github.com>
2026-03-11 21:49:35 +0900

    feat: 배포 버전 수정

--- app/api/axios/axiosInstance.ts
+++ app/api/axios/axiosInstance.ts
@@ -1,27 +1,9 @@
-// import axios from "axios";
-
-// const baseURL =
-//   process.env.NODE_ENV === "development"
-//     ? "http://13.239.246.72:8080/api"
-//     : "/proxy";
-
-// export const axiosInstance = axios.create({
-//   baseURL,
-// });
-
-// axiosInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return config;
-// });
-
 import axios from "axios";
 
-const baseURL = "http://13.239.246.72:8080/api";
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://13.239.246.72:8080/api"
+    : "/proxy";
 
 export const axiosInstance = axios.create({
   baseURL,
@@ -36,3 +18,21 @@ axiosInstance.interceptors.request.use((config) => {
 
   return config;
 });
+
+// import axios from "axios";
+
+// const baseURL = "http://13.239.246.72:8080/api";
+
+// export const axiosInstance = axios.create({
+//   baseURL,
+// });
+
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("accessToken");
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
