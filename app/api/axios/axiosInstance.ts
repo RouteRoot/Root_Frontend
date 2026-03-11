e00a153302b9197f2effabd29de4dd60dabb340d
min3eo <110177217+min3eo@users.noreply.github.com>
2026-03-11 11:32:29 +0900

    fix: Axios Instance 구조 수정

--- app/api/axios/axiosInstance.ts
+++ app/api/axios/axiosInstance.ts
@@ -2,7 +2,14 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "http://3.106.248.91:8080/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
