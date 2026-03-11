d6a0e0fcb4380d2a61c02d4736cf649ab8d4a10a
min3eo <110177217+min3eo@users.noreply.github.com>
2026-03-12 00:22:23 +0900

    fix: 불필요한 주석 제거

--- app/api/axios/axiosInstance.ts
+++ app/api/axios/axiosInstance.ts
@@ -19,20 +19,3 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
-// import axios from "axios";
-
-// const baseURL = "http://13.239.246.72:8080/api";
-
-// export const axiosInstance = axios.create({
-//   baseURL,
-// });
-
-// axiosInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return config;
-// });
