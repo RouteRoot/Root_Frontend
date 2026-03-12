84f1ecc8fb1f49eeb1283d8f480c6ac94553192c
min3eo <110177217+min3eo@users.noreply.github.com>
2026-03-12 13:57:08 +0900

    fix: 주소 변경

--- app/api/axios/axiosInstance.ts
+++ app/api/axios/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://13.239.246.72:8080/api"
+    ? "http://13.239.246.72:8080/api/"
     : "api/";
 
 export const axiosInstance = axios.create({
