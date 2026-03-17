5b42bbb2919aba2780768356fbb42cf1ab70a19c
min3eo <110177217+min3eo@users.noreply.github.com>
2026-03-17 14:24:15 +0900

    fix: 로그인 경로 수정

--- app/api/axios/axiosInstance.ts
+++ app/api/axios/axiosInstance.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
 export const axiosInstance = axios.create({
-  baseURL,
+  baseURL: "/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("accessToken");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
 
   return config;
